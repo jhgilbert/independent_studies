b23a163ec4253886b8e2d8700a429d155f318ca9
Jen Gilbert <jgilbert@arubanetworks.com>
2013-11-29 01:04:40 -0800

    Successfully create user with OpenID login

--- independent_studies_api/app/controllers/sessions_controller.rb
+++ independent_studies_api/app/controllers/sessions_controller.rb
@@ -2,11 +2,25 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
 
   def create
-  	auth = RestClient.post 'https://rpxnow.com/api/v2/auth_info', :token => params[:token], :apiKey => RPX_API_KEY
-  	puts auth
+  	auth = JSON.parse(RestClient.post 'https://rpxnow.com/api/v2/auth_info', :token => params[:token], :apiKey => RPX_API_KEY)
+  	current_user = User.find_by_identifier(auth['profile']['identifier'])
+  	if current_user == nil
+  		new_user = User.create(
+  			:identifier => auth['profile']['identifier'],
+  			:first_name => auth['profile']['name']['givenName'],
+  			:last_name => auth['profile']['name']['familyName'],
+  			:email => auth['profile']['email'] 
+  		)
+  		new_user.save
+  		current_user = new_user
+  	else
+  		session[:id] = current_user.id
+  	end
   	redirect_to root_url
   end
 
   def destroy
+  	# remove the user's ID from the session
+  	# redirect to the home page
   end
 end
--- independent_studies_api/db/schema.rb
+++ independent_studies_api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129044213) do
+ActiveRecord::Schema.define(version: 20131129085050) do
 
   create_table "courses", force: true do |t|
     t.string   "author"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20131129044213) do
     t.string   "difficulty"
     t.boolean  "free"
     t.string   "publisher"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "identifier"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "zipcode"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
