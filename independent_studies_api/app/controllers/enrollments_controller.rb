96b39f802c5898b27553de77ab5a451ab22e15f6
Jen Gilbert <jgilbert@arubanetworks.com>
2013-11-29 14:44:14 -0800

    Sketch in enrollment creation process

--- independent_studies_api/app/controllers/enrollments_controller.rb
+++ independent_studies_api/app/controllers/enrollments_controller.rb
@@ -1,2 +1,16 @@
 class EnrollmentsController < ApplicationController
+	def create
+		@enrollment = Enrollment.new
+		@enrollment.user_id = session[:id]
+		@enrollment.course_id = params[:enrollment][:course_id]
+		@enrollment.percentage = 0
+		@enrollment.save
+		render json: @enrollment
+	end
+
+	private
+
+	def enrollment_params
+		params.require(:enrollment).permit(:course_id)
+	end
 end
