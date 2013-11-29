2a6225c3db2e66f1fd6643645e50e37a6c25503a
Jen Gilbert <jgilbert@arubanetworks.com>
2013-11-29 14:28:06 -0800

    Adjust CSRF approach to accommodate static app

--- independent_studies_api/public/js/app.js
+++ independent_studies_api/public/js/app.js
@@ -1,20 +1,17 @@
 var independentStudies = angular.module('independentStudies', []);
 
-independentStudies.config([
-    "$httpProvider", function ($httpProvider) {
-        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-    }
-]);
-
 // Top-level controller
 function mainCtrl($scope, $http) {
     $scope.navControls = {selectedPanel: 'dashboard'};
     $scope.sessionData = {name: null, userIsLoggedIn: false};
-    $http.get('/sessions').success(function(data) {
+    $http.get('/sessions').success(function (data) {
         $scope.sessionData.name = data.name;
+        CSRF_TOKEN = data.token;
         if ($scope.sessionData.name !== null) {
             $scope.sessionData.userIsLoggedIn = true;
         }
     });
 }
+
+
 
