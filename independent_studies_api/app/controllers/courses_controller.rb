26d43bab53a648ca56c22accf091428834bd3d32
Jen Gilbert <jgilbert@arubanetworks.com>
2013-11-28 21:44:41 -0800

    Sketch in basic courses API

--- independent_studies_api/app/controllers/courses_controller.rb
+++ independent_studies_api/app/controllers/courses_controller.rb
@@ -1,2 +1,29 @@
 class CoursesController < ApplicationController
+	def index
+		@courses = Course.all
+		render json: @courses
+	end
+
+	def create
+		@course = Course.new(params[:course])
+		@course.save
+		render json: @course
+	end
+
+	def update
+		@course = Course.find(params[:course][:id])
+		@course.author = params[:course][:author]
+		@course.title = params[:course][:title]
+		@course.description = params[:course][:description]
+		@course.free = params[:course][:free]
+		@course.difficulty = params[:course][:difficulty]
+		@course.publisher = params[:course][:publisher]
+		@course.save
+		render json: @course
+	end
+
+	def delete
+		@course = Course.find(params[:course][:id])
+		@course.destroy!
+	end
 end
