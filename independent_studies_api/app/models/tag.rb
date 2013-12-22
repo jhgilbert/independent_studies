9945284f7ff3cbc207d35da9cf61d0fba0adb754
Jen Gilbert <jgilbert@arubanetworks.com>
2013-12-22 15:02:53 -0800

    Finish implementation of really terrible tag-editing system

--- independent_studies_api/app/models/tag.rb
+++ independent_studies_api/app/models/tag.rb
@@ -1,5 +1,4 @@
 class Tag < ActiveRecord::Base
-	validates :text, :uniqueness => true
-	has_many :tag_associations
+	has_many :tag_associations, dependent: :destroy
 	has_many :courses, -> { distinct }, through: :tag_associations
 end
