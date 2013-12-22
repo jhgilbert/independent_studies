cb3bed8d3f8eaf72380a88122a90fcf2abe488e3
Jen Gilbert <jgilbert@arubanetworks.com>
2013-12-21 23:26:09 -0800

    Fix security issue

--- independent_studies_api/app/controllers/application_controller.rb
+++ independent_studies_api/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
-  # after_filter :set_csrf_cookie_for_ng
+  after_filter :set_csrf_cookie_for_ng
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
